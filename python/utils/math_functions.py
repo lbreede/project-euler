c225e3742f749fc681034aac98cc9c23f8cdb37e
lbreede <lennart.breede@gmail.com>
2022-01-11 15:57:54 -0800

    solved p042 and still stuck at p012

--- python/utils/math_functions.py
+++ python/utils/math_functions.py
@@ -1,10 +1,5 @@
 from math import sqrt
 
-# def mrange(start, stop, step):
-#     while start < stop:
-#         yield start
-#         start += step
-
 def is_prime(num):
     """Checks if parsed number is a prime number.
 
@@ -20,3 +15,15 @@ def is_prime(num):
         return False
     # return all(num % i for i in mrange(3, int(sqrt(num)) + 1, 2))
     return all(num % i for i in range(3, int(sqrt(num)) + 1, 2))
+
+def triangle(n):
+    """Returns the triangle number of n using tn = ½n(n+1)
+
+    Args:
+        n: The number
+
+    Returns:
+        The triangle number of n
+
+    """
+    return int(0.5*n * n + 0.5*n)
