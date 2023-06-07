228d932285c8a898385f782a09eb15040b002265
lbreede <lennart.breede@gmail.com>
2023-06-07 12:58:49 -0700

    delete python directory

--- python/p013.py
+++
@@ -1,5 +0,0 @@
-with open("p013_numbers.txt", "r") as f:
-    numbers = list(map(int, f.read().split("\n")))
-
-result = str(sum(numbers))[:10]
-print(result)
--- python/p016.py
+++
@@ -1,1 +0,0 @@
-print(sum([int(x) for x in str(pow(2, 1000))]))
