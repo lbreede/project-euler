a6331dbeab7e6832462eb4eade5561002a76bdc9
lbreede <lennart.breede@gmail.com>
2023-06-07 14:48:47 -0700

    2023-06-07T14:48

---
+++ src/p007.py
@@ -0,0 +1,14 @@
+from utils.math_functions import is_prime
+
+i = 2
+primes_found = 0
+while True:
+    if is_prime(i):
+        primes_found += 1
+        if primes_found == 10001:
+            result = i
+            break
+    i += 1
+
+print("Project Euler - Sum Square Difference (Problem 7)")
+print(f"Answer: {result:,}")
