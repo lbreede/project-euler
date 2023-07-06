d51174d65e1a788af6704fffbf94ad4ffeaf00d6
lbreede <lennart.breede@gmail.com>
2023-07-05 22:10:47 -0700

    removed docstrings

--- python/p006.py
+++ python/p006.py
@@ -1,39 +1,12 @@
 def sum_of_squares(n: int) -> int:
-    """Returns the sum of the squares of the first n natural numbers.
-
-    Args:
-        n (int): The number of natural numbers to sum.
-
-    Returns:
-        int: The sum of the squares of the first n natural numbers.
-
-    """
     return sum([x * x for x in range(1, n + 1)])
 
 
 def square_of_sums(n: int) -> int:
-    """Returns the square of the sum of the first n natural numbers.
-
-    Args:
-        n (int): The number of natural numbers to sum.
-
-    Returns:
-        int: The square of the sum of the first n natural numbers.
-
-    """
     return sum(range(1, n + 1)) ** 2
 
 
 def sum_square_difference(n: int) -> int:
-    """Returns the difference between the square of the sum and the sum of the squares
-    of the first n natural numbers.
-
-    Args:
-        n (int): The number of natural numbers to sum.
-
-    Returns:
-           int: The difference between the square of the sum and the sum of the squares
-            of the first n natural numbers."""
     return square_of_sums(n) - sum_of_squares(n)
 
 
